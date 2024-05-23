7b3cce6fffa13eb6a1e9eaba0b31032033f29e2f
master_lin <avnk.24598@gmail.com>
2024-05-23 14:05:45 +0530

    a

--- client/src/components/proj/Project.jsx
+++ client/src/components/proj/Project.jsx
@@ -4,22 +4,18 @@ import "./Project.css"
 const Project = () => {
   return (
     <div className='project-main'>
-      <div className='project-list-div'>
-        <ul className='project-list'>
-          <li className='project-list-item'>
-            <div><p>Alzheimer project</p></div>
-          </li>
-          <li className='project-list-item'>
-            <div><p>android calculator app</p></div>
-          </li>
-          <li className='project-list-item'>
-            <div><p>Medi sync website</p></div>
-          </li>
-        </ul>
-      </div>
       <div className='description'>
         <h2>Projects</h2>
       </div>
+      <div className='project-list-div'>
+        
+        <div><p>Alzheimer project</p></div>
+        <div><p>android calculator app</p></div>
+        <div><p>Medi sync website</p></div>
+        <div><p>Medi sync website</p></div>
+        <div><p>Medi sync website</p></div>
+      </div>
+      
     </div>
   )
 }
--- client/src/components/skill/Skill.jsx
+++ client/src/components/skill/Skill.jsx
@@ -10,26 +10,20 @@ const Skill = () => {
         <div className="heading">
             <h2>Skills</h2>
         </div>
-        <div className="carousel enable-animation">
-
-            <ul className='list-skill marquee__content'>
-              <li className="list-item-skill li1">
-                <div><p>Java</p></div>
-              </li>
-              <li className="list-item-skill li2">
-                <div><p>SQL</p></div>
-              </li>
-              <li className="list-item-skill li3">
-                <div><p>Python</p></div>
-              </li>
-              <li className="list-item-skill li4">
-                <div><p>React</p></div>
-              </li>
-              <li className="list-item-skill li5">
-                <div><p>Django</p></div>
-              </li>
-            </ul>
-          
+        <div className="carousel">
+            <div className="row-list">
+            <div><p>Java</p></div>
+            <div><p>SQL</p></div>
+            <div><p>Python</p></div>
+            <div><p>React</p></div>
+            <div><p>Django</p></div>
+            <div><p>Django</p></div>
+            <div><p>Django</p></div>
+            <div><p>Django</p></div>
+            <div><p>Django</p></div>
+            <div><p>Django</p></div>
+            <div><p>Django</p></div>
+          </div>        
         </div>
     </div>
   )
