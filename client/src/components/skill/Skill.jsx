12befc504b38ce2f750d6c8704014c352cd84fef
avnk-342 <avnk.24598@gmail.com>
2024-06-11 15:03:05 +0530

    added tailwind and completed carousel for skill

--- client/src/components/skill/Skill.jsx
+++ client/src/components/skill/Skill.jsx
@@ -8,12 +8,12 @@ const Skill = () => {
 
   const btnNext = () =>{
     let box = document.querySelector(".row-list")
-    box.scrollLeft = box.scrollLeft + 70;
+    box.scrollLeft = box.scrollLeft + 250;
   }
 
   const btnPrev = () =>{
     let box = document.querySelector(".row-list")
-    box.scrollLeft = box.scrollLeft - 70;
+    box.scrollLeft = box.scrollLeft - 250;
   
     console.log()
   }
@@ -24,24 +24,26 @@ const Skill = () => {
         <div className="heading">
             <h2>Skills</h2>
         </div>
+        
         <div className="carousel">
-          <button className='nextbtn' onClick={btnNext}>Next</button>
-          <div className="row-list">
-            <div><p className='testdiv'>Java</p></div>
-            <div><p>SQL</p></div>
-            <div><p>Python</p></div>
-            <div><p>React</p></div>
-            <div><p>Django</p></div>
-            <div><p>Django</p></div>
-            <div><p>Django</p></div>
-            <div><p>Django</p></div>
-            <div><p>Django</p></div>
-            <div><p>Django</p></div>
-            <div><p>Django</p></div>
-            <div><p>LOLOL</p></div>
-          </div>
-          <button className='prevbtn' onClick={btnPrev}>Prev</button>        
+        <button className='nextbtn' onClick={btnNext}>Next</button>
+          <ul className="row-list">
+            <li><div className='skill-card'><p className='testdiv'>Java</p></div></li>
+            <li><div className='skill-card'><p>SQL</p></div></li>
+            <li><div className='skill-card'><p>Python</p></div></li>
+            <li><div className='skill-card'><p>React</p></div></li>
+            <li><div className='skill-card'><p>Django</p></div></li>
+            <li><div className='skill-card'><p>Django</p></div></li>
+            <li><div className='skill-card'><p>Django</p></div></li>
+            <li><div className='skill-card'><p>Django</p></div></li>
+            <li><div className='skill-card'><p>Django</p></div></li>
+            <li><div className='skill-card'><p>Django</p></div></li>
+            <li><div className='skill-card'><p>Django</p></div></li>
+            <li><div className='skill-card'><p>LOLOL</p></div></li>
+          </ul>
+          <button className='prevbtn' onClick={btnPrev}>Prev</button>  
         </div>
+       
     </div>
   )
 }
