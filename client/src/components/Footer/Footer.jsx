713d57e3b007d9b98dcda73994f3a2988a4f71e3
avnk-342 <avnk.24598@gmail.com>
2024-08-24 00:19:07 +0530

    added footer

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Footer.css'
+const Footer = () => {
+  return (
+    
+    <div className='footer'>
+
+        <div className="main-up">
+            <div className="logo">
+                <h1>Anmol Koundal</h1>
+            </div>
+            <hr />
+            <ul className='footer-list'>
+                <li><p>avnk.24598@gmail.com</p></li>
+                <li><p>Git-Hub: avnk-342</p></li>
+                <li><p>Kharghar, Navi Mumbai</p></li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -1,17 +1,15 @@
 import React from 'react'
 import Hero from "../components/hero/Hero"
 import Skill from '../components/skill/Skill'
-import Footer from '../components/footer/Footer'
+import Footer from '../components/Footer/Footer'
 import Project from '../components/proj/Project'
-// import Standing from '../components/standings/Standing'
+
 const Main = () => {
   return (
     <div>
       <Hero/>
       <Skill/>
-      
       <Project/>
-      {/* <Standing/> */}
       <Footer/>
     </div>
   )
